a4cbeb7459dd05f7da4da9eb94f0095bf29db28c
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-09-07 16:30:47 -0300

    Atualização dos métodos usando ProdutoDAOEntity

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -25,12 +25,11 @@ namespace Alura.Loja.Testes.ConsoleApp
             RecuperarProdutos();
 
             //atualizar o produto
-            using(var contexto = new LojaContext())
+            using(var contexto = new ProdutoDAOEntity())
             {
-                Produto primeiroProduto = contexto.Produtos.First();
+                Produto primeiroProduto = contexto.Produtos().First();
                 primeiroProduto.Nome = "DVD 007: Cassino Royale";
-                contexto.Produtos.Update(primeiroProduto);
-                contexto.SaveChanges();
+                contexto.Atualizar(primeiroProduto);
             }
             RecuperarProdutos();
         }
@@ -38,23 +37,22 @@ namespace Alura.Loja.Testes.ConsoleApp
         //deleta um produto do banco de dados
         private static void ExcluirProdutos()
         {
-            using(var contexto = new LojaContext())
+            using(var contexto = new ProdutoDAOEntity())
             {
-                IList<Produto> produtos = contexto.Produtos.ToList();
+                IList<Produto> produtos = contexto.Produtos();
                 foreach(var produto in produtos)
                 {
-                    contexto.Produtos.Remove(produto);
+                    contexto.Remover(produto);
                 }
-                contexto.SaveChanges();
             }
         }
 
         //select usando o entity framework
         private static void RecuperarProdutos()
         {
-            using(var contexto = new LojaContext())
+            using(var contexto = new ProdutoDAOEntity())
             {
-                IList<Produto> produtos = contexto.Produtos.ToList();
+                IList<Produto> produtos = contexto.Produtos();
                 Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
                 foreach (var produto in produtos)
                 {
@@ -68,34 +66,29 @@ namespace Alura.Loja.Testes.ConsoleApp
         //cria um produto no banco de dados
         private static void GravarUsandoEntity()
         {
-            //Produto p = new Produto();
-            //p.Nome = "Harry Potter e a Ordem da Fênix";
-            //p.Categoria = "Livros";
-            //p.Preco = 19.89;
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
 
-            //Produto p2 = new Produto();
-            //p2.Nome = "O Senhor dos Anéis - A Sociedade do Anel(Volume 1)";
-            //p2.Categoria = "Livros";
-            //p2.Preco = 24.90;
+            Produto p2 = new Produto();
+            p2.Nome = "O Senhor dos Anéis - A Sociedade do Anel(Volume 1)";
+            p2.Categoria = "Livros";
+            p2.Preco = 24.90;
 
-            //Produto p3 = new Produto();
-            //p3.Nome = "O Monge e o Executivo";
-            //p3.Categoria = "Livros";
-            //p3.Preco = 30.99;
+            Produto p3 = new Produto();
+            p3.Nome = "O Monge e o Executivo";
+            p3.Categoria = "Livros";
+            p3.Preco = 30.99;
 
             Produto p4 = new Produto();
             p4.Nome = "007: Cassino Royale";
             p4.Categoria = "Filme";
             p4.Preco = 49.90;
 
-            using (var contexto = new LojaContext())
+            using (var contexto = new ProdutoDAOEntity())
             {
-                //contexto.Produtos.Add(p);
-
-                //adiciona uma lista de objetos; melhor performance para inclusão de muitos objetos
-                //contexto.Produtos.AddRange(p2, p3);
-                contexto.Produtos.Add(p4);
-                contexto.SaveChanges();
+                contexto.Adicionar(p4);
             }
         }
 

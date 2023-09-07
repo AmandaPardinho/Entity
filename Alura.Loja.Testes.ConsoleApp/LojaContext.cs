c497f4670a06a76e3ceb3cceaeed43daa15637ea
AmandaPardinho <amanda.pardinho@performait.com>
2023-09-07 13:40:30 -0300

    Criação do construtor LojaContext e alteração de um método

--- Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -10,11 +10,23 @@ namespace Alura.Loja.Testes.ConsoleApp
         //Propriedade (produtos): possui o mesmo nome da tabela do banco de dados; representa o conjunto de objetos da classe 
         public DbSet<Produto> Produtos { get; set; }
 
+        public LojaContext()
+        {
+            
+        }
+
+        //injeta dependência
+        //recebe as opções de configuração de conexão com o banco de dados 
+        public LojaContext(DbContextOptions<LojaContext> options): base(options) { }
+
         //define qual o banco de dados usado e o seu endereço
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //construtor de opções
-            optionsBuilder.UseSqlServer("Server=DESKTOP-MIHQT5G;Database=Loja;user=sa;password=amanda03;Trusted_Connection=true;TrustServerCertificate=True");
+            if(!optionsBuilder.IsConfigured)
+            {
+                //construtor de opções
+                optionsBuilder.UseSqlServer("Server=DESKTOP-MIHQT5G;Database=Loja;user=sa;password=amanda03;Trusted_Connection=true;TrustServerCertificate=True");
+            }
         }
     }
 }

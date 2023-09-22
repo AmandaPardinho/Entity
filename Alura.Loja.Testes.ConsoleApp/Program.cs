db007dc7723743fcd3161b16419640610ba3d1ab
AmandaPardinho <amanda.m.pardinho@gmail.com>
2023-09-22 16:12:58 -0300

    Inserção de comentários e novos métodos

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,99 +13,61 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //GravarUsandoAdoNet();
-            //GravarUsandoEntity();
-            //RecuperarProdutos();
-            //ExcluirProdutos();
-            //RecuperarProdutos();
-            AtualizarProduto();
-        }
-
-        private static void AtualizarProduto()
-        {
-            //incluir um produto
-            GravarUsandoEntity();
-            RecuperarProdutos();
-
-            //atualizar o produto
-            using(var contexto = new ProdutoDAOEntity())
+            using(var contexto = new LojaContext())
             {
-                Produto primeiroProduto = contexto.Produtos().First();
-                primeiroProduto.Nome = "DVD 007: Cassino Royale";
-                contexto.Atualizar(primeiroProduto);
-            }
-            RecuperarProdutos();
-        }
+                //gerando SQL através do Entity
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
 
-        //deleta um produto do banco de dados
-        private static void ExcluirProdutos()
-        {
-            using(var contexto = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = contexto.Produtos();
-                foreach(var produto in produtos)
+                var produtos = contexto.Produtos.ToList();
+
+                //foreach(var produto in produtos)
+                //{
+                //    Console.WriteLine(produto);
+                //}
+
+                //contexto herda de DbContext(classe base de toda a API do Entity) => possui o ChangeTracker => responsável por rastrear todas as mudanças que estão acontecendo numa determinada instância do contexto => possui uma lista(recuperada através do Entries) de todas as entidades que estão sendo gerenciadas num dado momento/contexto
+
+                ExibeEntries(contexto);
+
+                //Classe EntityEntry => retornada para cada um dos objetos disponíveis no banco de dados; possui uma propriedade essencial para o monitoramento de mudanças - a propriedade estado(registra o estado da entidade); caso haja uma alteração no estado, o método SaveChanges passa a ser requerido
+
+                //var p1 = produtos.Last();
+                //p1.Nome = "007: Cassino Royale";
+
+                var novoProduto = new Produto()
                 {
-                    contexto.Remover(produto);
-                }
-            }
-        }
+                    Nome = "Um Dia",
+                    Categoria = "Filme",
+                    Preco = 52.00
+                };
+                contexto.Produtos.Add(novoProduto);
 
-        //select usando o entity framework
-        private static void RecuperarProdutos()
-        {
-            using(var contexto = new ProdutoDAOEntity())
-            {
-                IList<Produto> produtos = contexto.Produtos();
-                Console.WriteLine("Foram encontrados {0} produto(s).", produtos.Count);
-                foreach (var produto in produtos)
+                Console.WriteLine("=================");
+                foreach (var e in contexto.ChangeTracker.Entries())
                 {
-                    Console.WriteLine(produto.Nome);
+                    Console.WriteLine(e.Entity.ToString() + " - " + e.State);
                 }
 
-                Console.ReadKey();
+                //ao acionar o SaveChanges, o Entity vai verificar qual estado teve alteração e acionar um comando SQL diferente de acordo com a alteração sofrida
+
+                contexto.SaveChanges();
+
+                //Console.WriteLine("=================");
+                //foreach (var produto in produtos)
+                //{
+                //    Console.WriteLine(produto);
+                //}
             }
         }
 
-        //cria um produto no banco de dados
-        private static void GravarUsandoEntity()
+        private static void ExibeEntries(LojaContext contexto)
         {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            Produto p2 = new Produto();
-            p2.Nome = "O Senhor dos Anéis - A Sociedade do Anel(Volume 1)";
-            p2.Categoria = "Livros";
-            p2.Preco = 24.90;
-
-            Produto p3 = new Produto();
-            p3.Nome = "O Monge e o Executivo";
-            p3.Categoria = "Livros";
-            p3.Preco = 30.99;
-
-            Produto p4 = new Produto();
-            p4.Nome = "007: Cassino Royale";
-            p4.Categoria = "Filme";
-            p4.Preco = 49.90;
-
-            using (var contexto = new ProdutoDAOEntity())
+            Console.WriteLine("=================");
+            foreach (var e in contexto.ChangeTracker.Entries())
             {
-                contexto.Adicionar(p4);
-            }
-        }
-
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            //DAO => data access object
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
             }
         }
     }

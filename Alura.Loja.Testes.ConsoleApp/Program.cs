820070fbc99fb969dbfde09c03d55356f6afdc46
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-10-02 06:58:20 -0300

    Código que simula uma compra de 6 pães

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,8 +14,31 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            
-            
+            //compra de 6 pães franceses
+            var paoFrances = new Produto();
+            paoFrances.Nome = "Pão Francês";
+            paoFrances.PrecoUnitario = 0.40;
+            paoFrances.Unidade = "Unidade";
+            paoFrances.Categoria = "Padaria";
+
+            var compra = new Compra();
+            compra.Quantidade = 6;
+            compra.Produto = paoFrances;
+            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
+
+            using(var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                //o Entity vai incluir por conta própria o produto "Pão Francês" => ele percebe que há uma referência não nula a um produto; vai incluir o objeto paoFrances sob a supervisão do change tracker
+                contexto.Compras.Add(compra);
+
+                MetodosAnteriores.ExibeEntries(contexto.ChangeTracker.Entries());
+
+                contexto.SaveChanges();
+            }
         }        
     }
 }

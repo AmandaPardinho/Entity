c08e366b766539311173ce6c866d9768c69625d2
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-10-02 07:27:26 -0300

    Adição de código de promoção

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,30 +14,23 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            //compra de 6 pães franceses
-            var paoFrances = new Produto();
-            paoFrances.Nome = "Pão Francês";
-            paoFrances.PrecoUnitario = 0.40;
-            paoFrances.Unidade = "Unidade";
-            paoFrances.Categoria = "Padaria";
+            var promocaoDePascoa = new Promocao();
+            promocaoDePascoa.Descricao = "Páscoa Feliz";
+            promocaoDePascoa.DataInicio = DateTime.Now;
+            promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
+            
+            promocaoDePascoa.Produtos.Add(new Produto());
+            promocaoDePascoa.Produtos.Add(new Produto());
+            promocaoDePascoa.Produtos.Add(new Produto());
 
-            var compra = new Compra();
-            compra.Quantidade = 6;
-            compra.Produto = paoFrances;
-            compra.Preco = paoFrances.PrecoUnitario * compra.Quantidade;
-
-            using(var contexto = new LojaContext())
+            using (var contexto = new LojaContext())
             {
                 var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
                 var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
                 loggerFactory.AddProvider(SqlLoggerProvider.Create());
+               
 
-                //o Entity vai incluir por conta própria o produto "Pão Francês" => ele percebe que há uma referência não nula a um produto; vai incluir o objeto paoFrances sob a supervisão do change tracker
-                contexto.Compras.Add(compra);
-
-                MetodosAnteriores.ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.SaveChanges();
+                
             }
         }        
     }

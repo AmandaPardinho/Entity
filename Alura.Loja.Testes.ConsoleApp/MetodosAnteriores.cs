257a938992a1a2b8457ff05da8d6055730273731
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-10-02 06:59:44 -0300

    Adição de mais métodos que seriam deletados

--- Alura.Loja.Testes.ConsoleApp/MetodosAnteriores.cs
+++ Alura.Loja.Testes.ConsoleApp/MetodosAnteriores.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -59,22 +63,22 @@ namespace Alura.Loja.Testes.ConsoleApp
             Produto p = new Produto();
             p.Nome = "Harry Potter e a Ordem da Fênix";
             p.Categoria = "Livros";
-            p.Preco = 19.89;
+            p.PrecoUnitario = 19.89;
 
             Produto p2 = new Produto();
             p2.Nome = "O Senhor dos Anéis - A Sociedade do Anel(Volume 1)";
             p2.Categoria = "Livros";
-            p2.Preco = 24.90;
+            p2.PrecoUnitario = 24.90;
 
             Produto p3 = new Produto();
             p3.Nome = "O Monge e o Executivo";
             p3.Categoria = "Livros";
-            p3.Preco = 30.99;
+            p3.PrecoUnitario = 30.99;
 
             Produto p4 = new Produto();
             p4.Nome = "007: Cassino Royale";
             p4.Categoria = "Filme";
-            p4.Preco = 49.90;
+            p4.PrecoUnitario = 49.90;
 
             using (var contexto = new ProdutoDAOEntity())
             {
@@ -87,12 +91,52 @@ namespace Alura.Loja.Testes.ConsoleApp
             Produto p = new Produto();
             p.Nome = "Harry Potter e a Ordem da Fênix";
             p.Categoria = "Livros";
-            p.Preco = 19.89;
+            p.PrecoUnitario = 19.89;
 
             //DAO => data access object
             using (var repo = new ProdutoDAO())
             {
                 repo.Adicionar(p);
+            }
+        }
+
+        //ChangeTracker
+        //using(var contexto = new LojaContext())
+        //{
+        //    //gerando SQL através do Entity
+        //    var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+        //    var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+        //    loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+        //                var produtos = contexto.Produtos.ToList();
+        //    ExibeEntries(contexto.ChangeTracker.Entries());
+
+        //    var novoProduto = new Produto()
+        //    {
+        //        Nome = "Um Dia",
+        //        Categoria = "Filme",
+        //        PrecoUnitario = 52.00
+        //    };
+        //    contexto.Produtos.Add(novoProduto);
+        //                ExibeEntries(contexto.ChangeTracker.Entries());
+
+        //    contexto.Produtos.Remove(novoProduto);
+
+        //                ExibeEntries(contexto.ChangeTracker.Entries());
+
+        //    //contexto.SaveChanges();
+
+        //    var entry = contexto.Entry(novoProduto);
+        //    //ExibeEntries(contexto.ChangeTracker.Entries());
+        //    Console.WriteLine(entry.Entity.ToString() + " - " + entry.State);
+        //}
+
+        public static void ExibeEntries(IEnumerable<EntityEntry> entries)
+        {
+            Console.WriteLine("=================");
+            foreach (var e in entries)
+            {
+                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
             }
         }
     }

258b857f3f16a41894e9fdd53757a124a4207aeb
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-10-04 09:52:37 -0300

    Criação do código para recuperar entidades de um relacionamento n para m

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -13,39 +14,15 @@ namespace Alura.Loja.Testes.ConsoleApp
     class Program
     {
         static void Main(string[] args)
-        {
-            using(var contexto = new LojaContext()) 
-            {
-                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
-                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(SqlLoggerProvider.Create());
-
-                var promocao = new Promocao();
-                promocao.Descricao = "Queima total janeiro 2024";
-                promocao.DataInicio = new DateTime(2024, 1, 1);
-                promocao.DataTermino = new DateTime(2024, 1, 31);
-
-                var produtos = contexto
-                    .Produtos
-                    .Where(p => p.Categoria == "Bebidas")
-                    .ToList();
-
-                foreach(var produto in produtos)
-                {
-                    promocao.IncluiProduto(produto);
-                }
-
-                contexto.Promocoes.Add(promocao);
-
-                MetodosAnteriores.ExibeEntries(contexto.ChangeTracker.Entries());
-
-                contexto.SaveChanges();
-            }
-
-            //o Entity e outras ORMs não recuperam as entidades relacionadas junto com SELECT que foi feito(comportamento padrão) => melhora a performance, já que impede que muitos objetos sejam recuperados de uma única vez
+        {           
             using(var contexto2 = new LojaContext())
             {
-                var promocao = contexto2.Promocoes.FirstOrDefault();
+                //recuperação de entidades em relacionamentos n para m
+                var promocao = contexto2
+                    .Promocoes
+                    .Include(p => p.Produtos)
+                    .ThenInclude(pp => pp.Produto)
+                    .FirstOrDefault();
 
                 Console.WriteLine("\nMostrando os produtos da promoção\n");
 

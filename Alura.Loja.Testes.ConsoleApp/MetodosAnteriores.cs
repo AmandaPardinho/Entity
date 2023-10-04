0d55b352b3b359b301fb15c435d38af50aecc2e7
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-10-04 10:32:34 -0300

    Arquivos criados e modificados automaticamente

--- Alura.Loja.Testes.ConsoleApp/MetodosAnteriores.cs
+++ Alura.Loja.Testes.ConsoleApp/MetodosAnteriores.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -223,6 +224,26 @@ namespace Alura.Loja.Testes.ConsoleApp
                 MetodosAnteriores.ExibeEntries(contexto.ChangeTracker.Entries());
 
                 contexto.SaveChanges();
+            }
+        }
+
+        private static void ExibeProdutosDaPromocao()
+        {
+            using (var contexto2 = new LojaContext())
+            {
+                //recuperação de entidades em relacionamentos n para m
+                var promocao = contexto2
+                    .Promocoes
+                    .Include(p => p.Produtos)
+                    .ThenInclude(pp => pp.Produto)
+                    .FirstOrDefault();
+
+                Console.WriteLine("\nMostrando os produtos da promoção\n");
+
+                foreach (var item in promocao.Produtos)
+                {
+                    Console.WriteLine(item.Produto);
+                }
             }
         }
     }
--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -14,23 +14,40 @@ namespace Alura.Loja.Testes.ConsoleApp
     class Program
     {
         static void Main(string[] args)
-        {           
-            using(var contexto2 = new LojaContext())
+        {
+            using(var contexto = new LojaContext())
             {
-                //recuperação de entidades em relacionamentos n para m
-                var promocao = contexto2
-                    .Promocoes
-                    .Include(p => p.Produtos)
-                    .ThenInclude(pp => pp.Produto)
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                //relacionamento um para um
+                var cliente = contexto
+                    .Clientes
+                    .Include(c => c.EnderecoEntrega)
                     .FirstOrDefault();
 
-                Console.WriteLine("\nMostrando os produtos da promoção\n");
+                Console.WriteLine($"Endereço de entrega: {cliente.EnderecoEntrega.Logradouro}");
 
-                foreach (var item in promocao.Produtos)
+                var produto = contexto
+                    .Produtos
+                    .Include(p => p.Compras)
+                    .Where(p => p.Id == 1)
+                    .FirstOrDefault();
+
+                contexto.Entry(produto)
+                    .Collection(p => p.Compras)
+                    .Query()
+                    .Where(c => c.Preco > 10)
+                    .Load(); //carregamento explícito
+                Console.WriteLine($"\nMostrando as compras do produto {produto.Nome}\n");
+                foreach (var item in produto.Compras)
                 {
-                    Console.WriteLine(item.Produto);
+                    Console.WriteLine(item);
                 }
             }
-        }        
+        }
+
+        
     }
 }
